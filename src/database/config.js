8ba18e46c71c9d6b27ea080e5313f86a21169ad0
John Niyontwali <nijohn0006@gmail.com>
2025-01-21 16:17:47 +0200

    added all changes related to database configurations

--- src/database/config.js
+++ src/database/config.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB_URL);
     console.log('DB connection successful');
   } catch (error) {
     console.error('DB connection failed:', error.message);
